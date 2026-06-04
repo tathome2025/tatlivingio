7eb306fac5be772d5df4797244a64df3665bf55c
tathome2025 <hello@tatliving.dev>
2026-06-05 02:53:51 +0800

    feat(palmvellum/app): rename capture → hotsync with palm in nav

---
+++ palmvellum/app/_app/immutable/entry/start.22JEeQUR.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/-VB8HIJm.js";import{y as t}from"../chunks/CoF0dGZ7.js";export{t as load_css,r as start};
--- palmvellum/app/_app/immutable/entry/start.njh26Lfr.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/nOwl05Q_.js";import{y as t}from"../chunks/SkC1-pZO.js";export{t as load_css,r as start};
