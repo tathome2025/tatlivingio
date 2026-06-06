5973b88fcf0b24e0b80df442b0836ea00c7544fb
tathome2025 <hello@tatliving.dev>
2026-06-06 21:29:52 +0800

    feat(palmvellum/app): add Japanese / Korean / Russian to language picker

--- palmvellum/app/_app/immutable/entry/start.DMpx1xaB.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/BtrCCRNE.js";import{y as t}from"../chunks/DXZNkYnP.js";export{t as load_css,r as start};
---
+++ palmvellum/app/_app/immutable/entry/start.POxCzlBw.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/DeyXF-cG.js";import{y as t}from"../chunks/Ct5f0f9f.js";export{t as load_css,r as start};
