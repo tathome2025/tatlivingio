baae98d0321431bc2c4c515dac5c96aa6891aff5
tathome2025 <hello@tatliving.dev>
2026-06-06 22:52:39 +0800

    deploy(palmvellum app): drop Palm enrollment card from Settings page

---
+++ palmvellum/app/_app/immutable/entry/start.Bk1Vj1F0.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/DsHHwfbt.js";import{y as t}from"../chunks/DmDDQdwj.js";export{t as load_css,r as start};
--- palmvellum/app/_app/immutable/entry/start.D5BH722_.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/7taSzUM_.js";import{y as t}from"../chunks/BqbFL0sp.js";export{t as load_css,r as start};
