b7029a2e36873ce7c8f7af52b3df1104e9e3062b
tathome2025 <hello@tatliving.dev>
2026-06-06 21:54:06 +0800

    docs(palmvellum): drop AAA-strict-limit, soften principle 06; rebuild app

---
+++ palmvellum/app/_app/immutable/entry/start.CAqslUFk.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/OjGZxmYo.js";import{y as t}from"../chunks/o4ywQG6b.js";export{t as load_css,r as start};
--- palmvellum/app/_app/immutable/entry/start.icgwmaz8.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/BrnlybuK.js";import{y as t}from"../chunks/Bb_NsQ8p.js";export{t as load_css,r as start};
