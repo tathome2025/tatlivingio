a2f6af9209e625ff5dae4a34529b87d774d2a506
tathome2025 <hello@tatliving.dev>
2026-06-07 02:37:22 +0800

    deploy(palmvellum app): clearer ics button wording, placed next to + add event manually

--- palmvellum/app/_app/immutable/entry/start.BNBiXxe6.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/BMbJWQQr.js";import{y as t}from"../chunks/tEX_EeIs.js";export{t as load_css,r as start};
---
+++ palmvellum/app/_app/immutable/entry/start.WMjdBXCf.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/D_D1oI60.js";import{y as t}from"../chunks/js7GjmDR.js";export{t as load_css,r as start};
