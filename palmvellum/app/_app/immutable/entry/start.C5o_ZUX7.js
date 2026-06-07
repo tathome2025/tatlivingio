5cd3a2be03617ffa8dfe507f31a9de131e8db9b9
tathome2025 <hello@tatliving.dev>
2026-06-07 09:17:30 +0800

    deploy(palmvellum app): drop completed todos from DateBook (bug fix)

---
+++ palmvellum/app/_app/immutable/entry/start.C5o_ZUX7.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/BmDiKxsu.js";import{y as t}from"../chunks/DJ2PCiw8.js";export{t as load_css,r as start};
--- palmvellum/app/_app/immutable/entry/start.Dkqzv67f.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/CIm1Q9un.js";import{y as t}from"../chunks/CZNLTwsz.js";export{t as load_css,r as start};
